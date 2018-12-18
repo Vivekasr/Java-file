6e7db92716c623bf67abd90f753fbf55341b9f78
Vivek Sharma <31165335+Vivekasr@users.noreply.github.com>
2018-12-18 23:50:22 +0530

    chapter2

---
+++ chapter2 excercise/drumtest/DrumKit.java
@@ -0,0 +1,13 @@
+class DrumKit {
+
+  boolean topHat = true;
+  boolean snare = true;
+
+  void playsnare() {
+    System.out.println("bang bang ba-bang");
+  }
+
+  void playTopHat() {
+    System.out.println("ding ding da-ding");
+  }
+}
