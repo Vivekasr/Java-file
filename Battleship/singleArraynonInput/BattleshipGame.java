7fb819b121340b8b19621dbd829615f6df37572f
Vivek Sharma <31165335+Vivekasr@users.noreply.github.com>
2018-12-23 20:06:48 +0530

    BattleShip
    single array battleship with input access but with bug.

--- Battleship/singleArraynonInput/BattleshipGame.java
+++ Battleship/singleArraynonInput/BattleshipGame.java
@@ -1,13 +1,25 @@
 class BattleshipGame {
 
   public static void main(String[] args) {
+    int numofGuesses = 0;
+    GameHelper helper = new GameHelper();
+
     Battleship battle = new Battleship();
+    int randomNum = (int) (Math.random() * 5);
 
-    int[] locations = {2,3,4};
+    int[] locations = {randomNum, randomNum+1, randomNum+2};
     battle.setLocationCells(locations);
+    boolean isAlive = true;
 
-    String userGuess = "2";
-    String result = battle.checkYourself(userGuess);
+    while (isAlive == true) {
+      String guess = helper.getUserInput("enter a number ");
+      String result = battle.checkYourself(guess);
+      numofGuesses++;
+      if (result.equals("kill")) {
+        isAlive = false;
+        System.out.println("You took " + numofGuesses + " guesses");
+      }
+    }
   }
-  
+
 }
